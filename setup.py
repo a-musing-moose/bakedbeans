08bf24132ea016e45428a06a6a64421d6099688d
Jonathan Moss <jmoss@snowballone.com.au>
2018-01-11 17:26:30 +1100

    adding all the pypi goodness

--- setup.py
+++ setup.py
@@ -1,26 +1,40 @@
+import io
+import os
+
 from setuptools import setup
 
+NAME = 'bakedbeans'
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), about)
 
 setup(
     name='bakedbeans',
-    version='0.1',
+    version=about['__version__'],
     author='Jonathan Moss',
     author_email='jmoss@commoncode.io',
-    packages=['bakedbeans'],
-    # note the below *must* be kept in sync with requirements.txt
+    url='https://github.com/a-musing-moose/bakedbeans',
+    license='MIT',
+    packages=[NAME],
     install_requires=[
         'click~=6.7',
         'Flask~=0.12.2',
         'jsonschema~=2.6.0'
     ],
     description='Canned response HTTP server',
+    long_description=long_description,
     classifiers=(
         'Intended Audience :: Developers',
         'Topic :: Utilities',
         'Programming Language :: Python :: 3.6',
     ),
-    entry_points='''
-        [console_scripts]
-        baked=bakedbeans.cli:main
-    '''
+    entry_points={
+        'console_scripts': ['baked=bakedbeans.cli:main'],
+    }
 )
